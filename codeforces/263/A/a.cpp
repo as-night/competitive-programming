a5d94376281614e12a7e7d79a416e0d8fd6c5900
as-night <as-night@users.noreply.github.com>
2024-08-09 23:23:40 +0200

    263A - Beautiful Matrix: Augment the indentation

--- codeforces/263/A/a.cpp
+++ codeforces/263/A/a.cpp
@@ -9,9 +9,11 @@ int main() {
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
             std::cin >> c;
+
             if (c == '1') {
                 std::cout << std::abs(2 - i) + std::abs(2 - j);
                 std::cout << '\n';
+
                 return EXIT_SUCCESS;
             }
         }
